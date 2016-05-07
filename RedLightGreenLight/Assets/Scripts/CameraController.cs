2c9a8203e209178ec54765e1a9f11763b0ba6fb5
Francesco Laddomada <francesco.laddomada@gmail.com>
2016-05-07 18:58:19 +0200

    other abandoned project

---
+++ RedLightGreenLight/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject Player1;
+	private GameObject Player2;
+
+
+	// Use this for initialization
+	void Start () {
+		Player1 = GameObject.Find ("Player1");
+		Player2 = GameObject.Find ("Player2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 ((Player1.transform.position.x-Player2.transform.position.x)/2f+Player2.transform.position.x, 0, -10f);
+	}
+}
---
+++ RedLightGreenLight/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int PlayerNumber;
+
+	private float MoveSpeed = 50.0f;
+	private float MaxSpeed = 100f;
+
+	private float JumpSpeed = 300.0f;
+	private bool Grounded = false;
+
+	private Rigidbody2D Body;
+
+	// Use this for initialization
+	void Start () {
+		Body = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update() {
+		Move ();
+		Jump ();
+	//	print (Body.velocity.magnitude);
+	}
+
+	void FixedUpdate () {
+
+	}
+
+	void Move() {
+		float HorizontalMovement = Input.GetAxis ("Horizontal") * MoveSpeed * Time.deltaTime;
+		Vector2 MoveForce = new Vector2 (HorizontalMovement, 0f);
+		Vector2 GroundVelocity = new Vector2 (Body.velocity.x, 0f);
+		if( GroundVelocity.magnitude < MaxSpeed )
+			Body.AddForce (MoveForce);
+	}
+
+	void Jump() {
+		if (Input.GetKeyDown (KeyCode.Space) && Grounded) {
+			Grounded = false;
+			Vector2 Force = transform.TransformDirection(Vector2.up) * JumpSpeed;
+			Vector3 RelativeForce = transform.InverseTransformDirection(Force);
+			Body.AddForce ( new Vector2(RelativeForce.x, RelativeForce.y) );
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Floor") {
+			Grounded = true;
+		}
+	}
+}
